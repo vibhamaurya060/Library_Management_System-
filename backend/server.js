9afffd409b9e2297680137cfafa4ff718dc36c35
Vibha Maurya <vibhamaurya060@gmail.com>
2025-03-18 22:31:51 +0530

    add-2

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port=process.env.PORT || 3000;
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://library-management-system-rho-lac.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
